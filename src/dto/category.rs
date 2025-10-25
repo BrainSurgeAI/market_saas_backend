c9157298743eea59479b09315e103c9802d64530
Ray Liu <liurui81@gmail.com>
2025-10-25 13:26:44 +0800

    refactor: restrict visibility from pub to pub(crate)

--- src/dto/category.rs
+++ src/dto/category.rs
@@ -3,27 +3,27 @@ use sqlx::{types::Json, FromRow};
 
 #[derive(Debug, Serialize, Deserialize, FromRow)]
 pub(crate) struct CategoryDTO {
-    pub id: i32,
+    pub(crate) id: i32,
 
     #[sqlx(rename = "level1_category")]
-    pub level_one_category: String,
+    pub(crate) level_one_category: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, FromRow)]
 pub(crate) struct CategoryWithSubCategoriesDTO {
     #[serde(rename = "id")]
-    pub category_id: i32,
+    pub(crate) category_id: i32,
 
     #[serde(rename = "category")]
-    pub category_name: String,
+    pub(crate) category_name: String,
 
     #[serde(rename = "subCategories")]
-    pub subcategories: Json<Vec<SubCategoryDTO>>,
+    pub(crate) subcategories: Json<Vec<SubCategoryDTO>>,
 }
 
 #[derive(Debug, Serialize, Deserialize, FromRow)]
 pub(crate) struct SubCategoryDTO {
-    pub id: i32,
+    pub(crate) id: i32,
 
-    pub name: String,
+    pub(crate) name: String,
 }
