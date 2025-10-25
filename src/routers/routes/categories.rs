5b9290af13e3dbce12d81a438c52dcb97f1629b8
Ray Liu <liurui81@gmail.com>
2025-10-25 13:20:17 +0800

    refact url of api

--- src/routers/routes/categories.rs
+++ src/routers/routes/categories.rs
@@ -2,12 +2,13 @@ use axum::{routing::get, Router};
 
 use crate::{
     repositories::my_sql_repository::MySqlRepository,
-    services::category_services::get_category_with_sub_categories,
+    services::category_services::get_categories_tree,
 };
 
+/// Get category with sub-categories route.
 pub fn categories_routes() -> Router {
     Router::new().route(
-        "/api/v1/category_with_sub_categories",
-        get(get_category_with_sub_categories::<MySqlRepository>),
+        "/api/v1/categories/tree",
+        get(get_categories_tree::<MySqlRepository>),
     )
 }
