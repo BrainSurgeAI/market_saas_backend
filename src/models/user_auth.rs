436aa3ef4bb1b599e52581ebb651faacf43b5c21
Ray Liu <liurui81@gmail.com>
2025-11-07 19:46:18 +0800

    remove fetching permissions and add real name to claims

--- src/models/user_auth.rs
+++ src/models/user_auth.rs
@@ -1,10 +1,7 @@
 #[derive(Debug, sqlx::FromRow)]
 pub(crate) struct UserPermission {
-    pub(crate) id: i32,
     pub(crate) real_name: String,
-    pub(crate) username: String,
     pub(crate) password_hash: String,
     pub(crate) is_super_admin: bool,
-    pub(crate) roles: String,
-    pub(crate) permissions: String,
+    pub(crate) roles: Option<String>
 }
